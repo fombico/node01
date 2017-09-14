5bde337d90c3fff5f293c798bd01d5e495036801
Fredrich Ombico <fombico@pivotal.io>
2017-09-13 23:00:41 -0400

    Simplified server

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
 var app = require('express')(),
 	port = process.env.PORT || 8080,
-	server = require('http').createServer(app),
-	morgan = require('morgan'),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
-	winston = require('winston'),
-	winstonMongo = require('winston-mongodb').MongoDb;
-
-winston.add(winston.transports.File, {filename: 'logs.log'});
-winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/winstontest'});
-
-mongoose.connect('mongodb://localhost/mongooseDemo', {
-  useMongoClient: true
-});
-
-app.use(morgan('combined')); // logging of requests
-app.use(bodyParser.urlencoded({extended: false})); // converts request body to json
-app.set('view engine', 'ejs');
+	server = require('http').createServer(app);
 
 app.get('/', (req, res, next) => {
-	var company = process.env.COMPANY || 'World';
-	res.send('Hello, ' + company + '!');
+	console.log("request received");
+	res.send('Hello, World');
 });
 
 app.listen(port, (err) => {
-	winston.log('info', 'listening on %s', port);
-	winston.log('info', 'foo', {foo:'bar'});
+	console.log('listening on %s', port);
 });
